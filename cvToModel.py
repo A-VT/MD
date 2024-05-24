e372bf5f30d0ce1f20f2bf116557b769c0774b06
Bernard A. Georges <pg53698@alunos.uminho.pt>
2024-05-24 09:29:09 +0100

    cv to model and some trys to fine tune a model

--- cvToModel.py
+++ cvToModel.py
@@ -1,6 +1,6 @@
-from backend import getMeSomeJuicyAnswers
 import fitz  # PyMuPDF
 import re
+from backend import getMeSomeJuicyAnswers
 
 def extract_text_from_pdf(pdf_path):
     text = ""
@@ -8,11 +8,6 @@ def extract_text_from_pdf(pdf_path):
         for page in doc:
             text += page.get_text()
     return text
-
-# Example usage
-pdf_path = 'cvs/cv.pdf'
-cv_text = extract_text_from_pdf(pdf_path)
-
 
 def preprocess_text(text):
     # Remove unwanted characters and extra spaces
@@ -22,6 +17,11 @@ def preprocess_text(text):
     text = text.lower()
     return text
 
-preprocessed_text = preprocess_text(cv_text)
-print(preprocessed_text)
-getMeSomeJuicyAnswers(preprocessed_text)
+# Example usage
+def getAnswersFromCV(pdf_path):
+    cv_text = extract_text_from_pdf(pdf_path)
+    preprocessed_text = preprocess_text(cv_text)
+    print(preprocessed_text)
+    getMeSomeJuicyAnswers(preprocessed_text)
+pdf_path = 'cvs/cv.pdf'
+getAnswersFromCV(pdf_path)
